0cbdc64b28dd2646eec5c13650004707f3f51c57
mu-142141 <mu-142141@AP086173.hrw.hs-weingarten.de>
2017-01-25 16:27:32 +0100

    test

--- service-layer/src/test/java/de/hrw/swep/votingservice/service/VotingServiceImplIntegrationTest.java
+++ service-layer/src/test/java/de/hrw/swep/votingservice/service/VotingServiceImplIntegrationTest.java
@@ -158,9 +158,11 @@ public class VotingServiceImplIntegrationTest {
 	public void testDeleteQuestion() {
 		List<Question> questions = votingService.getAllQuestions();
 		Question question = questions.iterator().next();
-		assertTrue(questions.contains(question));
+		votingService.deleteQuestion(question);
+		assertEquals(4, votingService.getAllQuestions().size());
 
-		// Ich weiﬂ leider nicht , wie man den Test korrekt  implemebtiert
+
+	
 	}
 
 	@Test
